061fdbe0fb239b339136b906ee6df10138ba7eae
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-09-25 14:09:50 +0200

    create unit tests base

--- App/PlotThatLine/PlotThatLineTests/obj/Debug/net8.0-windows/PlotThatLineTests.AssemblyInfo.cs
+++ App/PlotThatLine/PlotThatLineTests/obj/Debug/net8.0-windows/PlotThatLineTests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PlotThatLine")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PlotThatLineTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e82307720e5f7c787d70bf6ca665279b1098a30")]
-[assembly: System.Reflection.AssemblyProductAttribute("PlotThatLine")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PlotThatLine")]
+[assembly: System.Reflection.AssemblyProductAttribute("PlotThatLineTests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PlotThatLineTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
